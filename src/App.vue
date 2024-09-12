905c8aff5fa7df773cb361e0c8566e018a38a247
Virginie <vb@ikimo9.com>
2024-09-12 12:25:49 +0200

    assets

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,38 @@
-<script setup></script>
+<script setup>
+import HomeIcon from './components/icons/IconHome.vue'
+import StarIcon from './components/icons/IconStar.vue'
+import SettingsIcon from './components/icons/IconSettings.vue'
+import SupportIcon from './components/icons/IconSupport.vue'
+import EmailIcon from './components/icons/IconEmail.vue'
+import NotificationsIcon from './components/icons/IconNotifications.vue'
+import SearchIcon from './components/icons/IconSearch.vue'
+import PublicAvatarIcon from './components/icons/IconPublicAvatar.vue'
+import PublicIcon from './components/icons/IconPublic.vue'
+import RemoveRedEyeIcon from './components/icons/IconRemoveRedEye.vue'
+import BrushIcon from './components/icons/IconBrush.vue'
+import FileCopyIcon from './components/icons/IconFileCopy.vue'
+import DeleteIcon from './components/icons/IconDelete.vue'
+import MoreHorizIcon from './components/icons/IconMoreHoriz.vue'
+</script>
 
 <template>
   <div>Ciril App</div>
+  <HomeIcon />
+  <StarIcon />
+  <SettingsIcon />
+  <SupportIcon />
+  <EmailIcon />
+  <NotificationsIcon />
+  <SearchIcon />
+  <PublicAvatarIcon />
+  <PublicIcon />
+  <RemoveRedEyeIcon />
+  <BrushIcon />
+  <FileCopyIcon />
+  <DeleteIcon />
+  <MoreHorizIcon />
+  <img src="./assets/__avatar_url.png" alt="avatar"/>
+  <img src="./assets/picture.png" alt="map"/>
 </template>
 
 <style scoped></style>
